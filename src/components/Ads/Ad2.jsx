fe6ebc627ab48599268e1522f9b015a490ec128f
Mihir6365 <mihir6365@gmail.com>
2023-07-12 20:48:04 +0530

    minor changes

--- src/components/Ads/Ad2.jsx
+++ src/components/Ads/Ad2.jsx
@@ -6,14 +6,16 @@ export default function Ad2() {
     adsbygoogle.push({});
   }, []);
   return (
-    <ins
-      className="adsbygoogle multi-ads"
-      style={{ display: "block", textAlign: "center" }}
-      data-ad-layout="in-article"
-      data-ad-format="fluid"
-      data-ad-client="ca-pub-7107200228987000"
-      data-ad-slot="9551463795"
-    />
+    <>
+      <ins
+        className="adsbygoogle multi-ads"
+        style={{ display: "block", textAlign: "center" }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-7107200228987000"
+        data-ad-slot="9551463795"
+      />
+    </>
   );
   // return (
   //   <ins
