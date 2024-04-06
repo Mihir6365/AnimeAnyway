90dbf08ccdba1803b1369b1fc9cce7e0c35ada7c
Mihir6365 <savla.mp@somaiya.edu>
2024-04-06 18:17:15 +0530

    ch6 complete

--- test.js
+++ test.js
@@ -1,246 +1,151 @@
 var inputString = `
-Directly after Sakayanagi conveyed her 5 picks as the attacker against Horikita in her second attack, Hashimoto stood up and approached Sakayanagi.
+The start of turn 15. Most of the classes are lined up closely now. 
 
-Instead of the usual faint smile, he had a stern expression on his face.
+First place is the Ichinose Class with 42 points. 
 
-It’s an act that really stands out, since everyone except Hashimoto are sitting in their seats.
+Second place are the Horikita and Sakayanagi classes, both at 40 points. 
 
-“Is something wrong, Hashimoto-kun?”
+Third place is the Ryuen Class with 39 points.
 
-“I thought I warned you last night. Does this mean you don’t find the information I gave you useful?”
+We’ve bottomed out on our savings from the first half. 
 
+We haven’t been pushed out yet, but we will be left behind if this trend continues.
 
+The Ryuen Class helped keep anxiety away at the start, but a dark cloud still looms over the class.
 
-Genre: Gourmet, difficulty 1.
+The class was driven into a corner where it could fall to last place at any time, depending on how things go.
 
-Attacker Targets: Koenji Rokusuke, Hasabe Haruka, Hirata Yosuke, Yukimura Teruhiko, Onodera Kayano.
+“Are you kidding me? Give me a break.”
 
+“I don’t want to be expelled!”
 
+“Me neither, you know!”
 
-He pointed at the names on the monitor behind him and showed his dissatisfaction.
+The students began to feel the danger after the number of eliminations increased to 4 after 1 more student got eliminated, and started causing a commotion.
 
-“Is that what it looks like to you?”
+In this situation, the others have no time to study either. Horikita pushed her chair and stood up.
 
-“Yeah, it is.”
+She has to start naming her targets now, and she calmly walked past the students making a commotion.
 
-“Well, your phone call yesterday was a little meddlesome. However, information is information. Of course I will not pointlessly ignore it after inscribing it in my hippocampus.”
+“No need to panic.”
 
-“Then… Why are you targeting Koenji?” 
+Horikita stood on the teacher’s platform and spoke to her classmates.
 
-“You told me the one target we must avoid in Class-B is Koenji, right?”
+“I know we’re in a terrible situation. There are currently 4 eliminations from our class. Ichinose-san took 1st place from us, and Ryuen-kun, who used to be in last place, is catching up extremely fast with consecutive perfect protections. I can no longer say that it’s certain we will win.” 
 
-“He has a contract with Horikita. So he’s always going to be protected and earn an unconditional point each time. I thought you would put my information to use here.” 
+It’s a different story if she can figure out Ryuen’s strategy and stop his perfect protections, but there’s little hope of that happening. 
 
-He wanted to be useful, but could not take it when that was quickly taken away from him. 
+There’s also little hope of stopping Ichinose’s high rate of successful protections.
 
-Kitou, sensing a difference in Hashimoto’s attitude compared to his usual cheerful demeanour, pulled a chair. 
+“What we can do now, is to keep fighting until the end.” 
 
-“You don’t need to worry about anything, Kitou-kun. Hashimoto-kun is just very dry.”
+She cannot give a guarantee of victory at this point. 
 
-After quietly laughing, Sakayanagi explained why she targeted Koenji despite the high possibility of him being protected. 
+However, in this special test where classes are competing with each other, the leader has to give some kind of guarantee.
 
-“I’m sure it’s true that there is a contract between him and Horikita-san. However, it’s only based on not expelling him, and the second point is that he is free to do as he likes, yes?”
+It’s no use to speak in a weak manner, nor is it useful to pointlessly put on airs.
 
-“Yeah…”
+The truth behind her words alone stabbed into the students’ hearts.
 
-“It’s a waste of a valuable protection to continue to protect him. It would be best to keep watching until he is targeted once and gets the problem wrong. That’s the least one can do in order to win, don’t you think?” 
+Horikita believes they can get through this. That’s what she conveyed to the students.
 
-“Horikita has a law-abiding personality, though. The class will be shaken if they see that she isn’t protecting him.”
+Yosuke, who would usually follow up after her, is now quietly listening to Horikita’s words.
 
-“If that’s enough to shake them, then they should be shaken anyway. Keeping the contract intact is important too, but I would rather doubt her character as a leader if she continued wasting a protection on Koenji-kun.”
+“Trust me.”
 
-The Horikita class decided their protections during the explanation, so the monitor’s screen changed.
+The class has to surpass this with a strong mentality. 
 
-There were no successful protections, and the 5 names Sakayanagi picked would all take on the assignment.
+This is the only choice they have.
 
-“How about that? As expected, she did not use a protection on Koenji-kun.”
+However, from looking at Horikita, I feel that there is also something else at play here.
 
-Hashimoto can’t keep hounding Sakayanagi about this after seeing the results.
+Turn 15. The Ichinose Class’s attack.
 
-“...Yeah, you’re right. What’s the point in going so far to take a point from Koenji, though? He’s weirdly smart, so there’s a higher chance for him to answer correctly than the other small fish, isn’t there?” 
 
-“I suppose so. He is a completely free person. As such, as long as he is promised this freedom by Horikita-san, he has no obligation to take the questions seriously. He might even get them wrong on purpose.”
 
-Sakayanagi spoke with conviction as if she could see the future.
+Defender Successful Protections: Karuizawa Ke, Satou Maya, Miyake Akito
 
-No way, Hashimoto thought, and he waited for the screen to change.
 
-As a result, Koenji got the question wrong as expected. One step closer to elimination.
 
-“It was a little risky, but we took 1 point. Impressive, Hime-san.” 
+Horikita successfully protected 3 students.
 
-Hashimoto was put at ease for the moment, but that quickly went even in the next turn. Because immediately after their next turn as the attacker began, Sakayanagi named Koenji right away.
+Further, the remaining 2 students answered correctly, giving the class a perfect score.
 
-She also picked the same genre, seemingly trying to show that she is targeting him on purpose.
+5 points to let the class breathe again. 
 
-After that, the students who heard the conversation also started making a commotion instead of just Hashimoto.
+However, the opponent’s persistent consecutive targeting did not cease.
 
-“Hey, what are you doing? Aren’t they going to protect Koenji if you pick the same genre too?”
+“W-What the hell..!?”
 
-Kamuro spoke up, confused by what Sakayanagi was doing.
+Overwhelmed by fear rather than joy, Kei averted her eyes from the monitor.
 
-“You’re not going to tell me he won’t be protected this time either, right..?”
+Including the students who have a faint grasp on the situation, even the students who don’t know anything started sensing something ominous in the face of this abnormality.
 
-“That’s how I see it. It’s the reason I named Koenji-kun again.”
+Horikita didn’t seem to be feeling that way after succeeding in her protections, and the next turn came. 
 
-Despite feeling that this was a foolish conjecture, the students stayed in their seats and waited as they looked at the screen.
+The Ichinose Class’s attack.
 
 
 
-Defender Successful Protections: None
+Defense Successful Protections: Karuizawa Kei, Nishimura Ryuuko
 
 
 
-“Seriously..? What the hell is Horikita thinking?”
+Two names this time, but Kei’s is also among them.
 
-Hashimoto grumbled in the face of the reality that Koenji was not protected this time either.
+“Stop… Why…”
 
-Then, the unthinkable act of Koenji answering incorrectly again.
+Ichinose is continuing to target and attack Karuizawa.
 
-“I’m not on Hashimoto’s side here, but why did you think they wouldn’t protect him this time either?”
+It’s natural to reach the conclusion that she is intentionally trying to expel her while specifically attacking her. 
 
-“Because of the same logic as the first time. He can answer incorrectly two times, so there is no need to protect him anyway. If she has to protect him, then she wants to wait until the final moment she has to do so. I’m sure Horikita-san did want him to answer correctly, though.”
+This move contradicts her image, but she isn’t stopping it.
 
-“...I see. So now, Horikita has to protect Koenji even if she doesn’t want to.”
+Turn 17. The Ichinose Class’s attack.
 
-Kitou muttered after being convinced by Sakayanagi’s words. 
 
-Horikita would not waste a protection on Koenji, as long as he still had the leeway for failure.
 
-In other words, Sakayanagi took a risk and continued targeting Koenji in order to waste her opponent’s protection in the later turns.
+Defense Successful Protections: Karuizawa Kei, Hirata Yosuke
 
-That’s the analysis.
 
-The fact that the two gourmet questions in a row were simple just couldn’t be helped.
 
-This is still the stage where each class is trying to determine how difficult every genre is.
+She’s still not stopping.
 
-“I’m sorry I doubted you, Princess. You had a plan, after all. But in that case, why not target Koenji from turn 1? That would destroy their protection slot for the remaining 8 turns. We lost 1 turn.”
+She won’t stop.
 
-“I was 99% sure she would not protect Koenji-kun, but the reason I waited until turn 2 is that I wanted to be completely sure. It was also an important move to lure them into making a mistake on turn 2. What if I attacked on turn 1 and Horikita-san decided to protect him after that? That would make things a lot more difficult for us.”
+No matter how many times she is blocked.
 
-There is a risk of being led around by a deceptive protection slot. 
+She will never stop naming her.
 
-She thought that Horikita would feel like there is some leeway if she continuously succeeded in protecting him, and that Horikita would gain control of the pace.
+“Why does she keep naming me..? Why..?”
 
-“So, thanks to him answering incorrectly on the first easy question, I was able to calculate that he would answer incorrectly the second time as well, and the results are great. It’s all thanks to the information you gave me.”
+Turn 18. The Ichinose Class’s attack.
 
-Hashimoto felt relieved after it was confirmed he was useful, and he returned to his seat after nodding.
 
-“Now, let’s finish things up with Koenji-kun.” 
 
-On turn 4, Sakayanagi surprised everyone by naming Koenji as her first target.
+Defense Successful Protections: Karuizawa Kei, Hasabe Haruka, Onodera Kayano
 
-“We can’t be too careful. I’m threatening that we can target him again any time if they show a gap in their defense. Thanks to your information, Hashimoto-kun, we know the inner circumstances of the Horikita class. However, she doesn’t believe that her contract with Koenji-kun has been leaked.” 
 
-“I see… This will make her feel that she has to use a protection slot of Koenji no matter what now.” 
 
-Sakayanagi chose Gourmet again, but she also raised the difficulty up to 2.
+Turn 19. The Ichinose Class’s attack.
 
-Hashimoto felt that it would be a loss if Koenji is protected, but he did not point it out.
 
-However, things developed in a mostly unexpected direction from there. 
 
+Defense Successful Protections: Karuizawa Kei
 
 
-Defender Successful Protections: Shinohara Satsuki, Sudou Ken.
 
+Turn 20. The Ichinose Class’s attack.
 
 
 
-Horikita made the unthinkable choice of not protecting Koenji.
+Defense Successful Protections: Karuizawa Kei, Sudou Ken
 
-“Why isn’t she protecting him?”
 
-“Maybe the information you gave me was fake?”
 
-“No way..! Horikita definitely made a promise to protect Koenji!”
+10 turns total in the second half.
 
-In the end, Koenji answered correctly and escaped elimination, but it was still very confusing.
-
-On one hand, Sakayanagi did not consume a protection slot, and Koenji answered correctly to avoid an elimination this time, which let her have a full grasp of the situation.
-
-“Does this mean Horikita abandoned Koenji..?”
-
-“This is our chance, then. We’ll crush him right now.”
-
-Kitou’s remark showed that he did not see this as a bad thing, but instead as a reason to keep targeting Koenji.
-
-“Yeah, that might be the way to go. It’ll lower their trust in Horikita and demoralize them.”
-
-Hashimoto saw the fact that Horikita chose not to use a protection slot as proof that the opponent class was in disarray.
-
-On the other hand, Sakayanagi reached a different conclusion.
-
-“I thought she would unconditionally protect him, or that he would be eliminated, but… It looks like Horikita-san has a different aim in her mind. We’re just going to make her happy if we keep targeting Koenji-kun now.”
-
-Sakayanagi chuckled and opened her cellphone
-
-“Still, I’m impressed that she’s thinking about ways to fight against me.”
-
-Sakayanagi started thinking. 
-
-Perhaps Ayanokoji was hiding behind Horikita.
-
-Was everything that happened so far a strategy built up under the guidance of someone?
-
-“For now, I’m pretty sure he’s not involved in this.” 
-
-If Ayanokoji was pulling the strings from behind the scene, such information would have made it to the other classes.
-
-Sakayanagi felt a strange, unusual and imperceptible presence.
-
-There was a hint of Ayanokoji’s thinking in Horikita’s actions.
-
-“She’s watching his back from more close up than anyone else, so I’d expect her to have grown this much, at least.”
-
-Sakayanagi can see where this is going. She won’t be overtaken by Horikita in this battle.
-
-“The problem is…”
-
-For Sakayanagi, who is in charge of Class-A, there is not one specific class to be wary of.
-
-The real problem is the possibility of 2, or even 3 classes joining hands in secret against her.
-
-That is the only concerning matter for Sakayanagi.
-
-She continued her spying and observations after the Special Test was announced, and did not detect any signs of such activities, nor did she receive any reports about them. 
-
-However, it’s very possible that the classes have joined hands in secret. 
-
-That’s precisely why the only way for her to determine whether there is cooperation or not is during the test, and she decided that the possibility of an alliance between the other classes is currently almost zero. 
-
-There is nothing unnatural about the other classes’ offense and defense.
-
-“Let us go for 1st place.”
-
-Sakayanagi had earned 29 points in total when the first half came to an end.
-
-It is a joyous thing to be in 1st place, but the shadow of Class-B chasing after her with a 1 point difference directly after still lingers.
-
-Hashimoto, without standing up, looked at the results shown on the monitor, and the remaining break time.
-
-“Masumi-san, shall we go for lunch? You were eliminated, so it won’t be an issue, right?” 
-
-“Fine by me, but you’re really inconsiderate when talking to people, you know.”
-
-It wasn’t a compliment, but Sakayanagi happily smiled and started walking while using her cane.
-
-Kitou quietly looked at the two and followed after them when they entered the corridor.
-
-“When did you invite him?”
-
-“I invited him over the phone.”
-
-“Hmm. So, are you sure you shouldn’t invite Hashimoto?”
-
-Hashimoto is often together with them when Kamuro and Kitou are seated at the same table. 
-
-It seems this caught Masumi’s attention.
-
-“I did invite him, but he refused. He also answered 2 problems incorrectly after being targeted by Ryuen-kun. It’s only natural he doesn’t want to be eliminated.”
-
-Kamuro imagined Hashimoto frantically looking things up on his phone, and a very faint smile appeared on her face.
+From the very beginning, Ichinose didn’t stop targeting Kei once.
 `;
 var fs = require("fs");
 function appendParagraphTags(str) {
@@ -257,7 +162,7 @@ function appendParagraphTags(str) {
 
 var modifiedString = `<p>` + appendParagraphTags(inputString) + "</p> \n </div>";
 // var modifiedString = `<p class="center italic">` + appendParagraphTags(inputString) + "</p> \n </div>";
-const filename = './public/assets/Year 2 Volume 10/Chapter 6_3.html';
+const filename = './public/assets/Year 2 Volume 10/Chapter 6_8.html';
 
 fs.readFile(filename, 'utf8', (err, data) => {
     if (err) {
